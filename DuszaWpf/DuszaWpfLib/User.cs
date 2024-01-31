5a9a939e1ccae3eb9faba4fc0139378ce0419efa
B4lint0413 <69457665+B4lint0413@users.noreply.github.com>
2024-01-31 15:17:37 +0100

    Initial
    Main classes

---
+++ DuszaWpf/DuszaWpfLib/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuszaWpfLib
+{
+    public abstract class User
+    {
+        protected User(string name, string password)
+        {
+            Name = name;
+            Password = password;
+        }
+        public string Name {get; init; }
+
+        private string Password { get; init; }
+    }
+}
