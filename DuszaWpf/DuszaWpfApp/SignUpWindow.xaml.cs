b255aba04a0f090badac1fe6617cd66d7872c232
B4lint0413 <69457665+B4lint0413@users.noreply.github.com>
2024-02-15 21:52:36 +0100

    Password Strength List UI

--- DuszaWpf/DuszaWpfApp/SignUpWindow.xaml.cs
+++ DuszaWpf/DuszaWpfApp/SignUpWindow.xaml.cs
@@ -20,10 +20,20 @@ namespace DuszaWpfApp
     /// </summary>
     public partial class SignUpWindow : Window
     {
+        private Dictionary<int, UIElement> levels = new Dictionary<int, UIElement>();
+        private Brush red = new SolidColorBrush(Colors.Red);
+        private Brush green = new SolidColorBrush(Colors.Green);
+
         public SignUpWindow()
         {
             WindowStartupLocation = WindowStartupLocation.CenterScreen;
             InitializeComponent();
+            levels.Add(16, Long);
+            levels.Add(8, Special);
+            levels.Add(4, Digit);
+            levels.Add(2, Upper);
+            levels.Add(1, Lower);
+
         }
 
         public void NavigateToLogIn(object sender, EventArgs e)
@@ -53,6 +63,25 @@ namespace DuszaWpfApp
             else
             {
                 MessageBox.Show("The confirmation password differs from the password!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public void StrengthLevel(object sender, RoutedEventArgs e)
+        {
+            Lower.Foreground = red;
+            Upper.Foreground = red;
+            Digit.Foreground = red;
+            Special.Foreground = red;
+            Long.Foreground = red;
+            int strengthLevel = Factory.StrengthCheck(passwd.Password);
+
+            foreach (var level in levels)
+            {
+                if (strengthLevel>=level.Key)
+                {
+                    ((TextBlock)level.Value).Foreground = green;
+                    strengthLevel -= level.Key;
+                }
             }
         }
 	}
