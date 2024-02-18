5b77a07f8e49327de0bab7dd4dea4b12fd74c60e
SzokolAttila <szokol.ati2005@gmail.com>
2024-02-18 16:12:25 +0100

    distributed tests related to events to new testClass

---
+++ DuszaTKV_BettingGame/DuszaTKVTest/EventTest.cs
@@ -0,0 +1,32 @@
+using DuszaTKVGameLib;
+using DuszaTKVGameLib.Exceptions;
+
+namespace DuszaTKVTest;
+
+[TestClass]
+public class EventTest
+{
+    [TestMethod]
+    public void TooLongEventNameOrSubjectNameThrowsException()
+    {
+        Assert.ThrowsException<LengthLimitExceededException>(() => new Event("asdfasdfasfdsafdsafasddfasddsf", "asd", "asd"));
+        Assert.ThrowsException<LengthLimitExceededException>(() => new Event("asd", "asdasdfasdfasdfsadfdsafdsafasd", "asd"));
+    }
+    
+    [TestMethod]
+    public void EmptyEventNameOrSubjectNameThrowsException()
+    {
+        Assert.ThrowsException<EmptyFieldException>(() => new Event("", "asd", ""));
+        Assert.ThrowsException<EmptyFieldException>(() => new Event("asd", "", ""));
+    }
+
+    [TestMethod]
+    public void ResultsToStringTest()
+    {
+        var events = new List<Event>();
+        events.Add(new Event("asdasd", "hululu", "game"));
+        var game = new Game("game", "organizer", events);
+        Assert.AreEqual("game\nhululu;asdasd;;0", game.ResultsToString());
+    }
+
+}
--- DuszaTKV_BettingGame/DuszaTKVTest/GameTest.cs
+++ DuszaTKV_BettingGame/DuszaTKVTest/GameTest.cs
@@ -28,29 +28,6 @@ public class GameTest
     }
 
     [TestMethod]
-    public void TooLongEventNameOrSubjectNameThrowsException()
-    {
-        Assert.ThrowsException<LengthLimitExceededException>(() => new Event("asdfasdfasfdsafdsafasddfasddsf", "asd", "asd"));
-        Assert.ThrowsException<LengthLimitExceededException>(() => new Event("asd", "asdasdfasdfasdfsadfdsafdsafasd", "asd"));
-    }
-    
-    [TestMethod]
-    public void EmptyEventNameOrSubjectNameThrowsException()
-    {
-        Assert.ThrowsException<EmptyFieldException>(() => new Event("", "asd", ""));
-        Assert.ThrowsException<EmptyFieldException>(() => new Event("asd", "", ""));
-    }
-    
-    [TestMethod]
-    public void ResultsToStringTest()
-    {
-        var events = new List<Event>();
-        events.Add(new Event("asdasd", "hululu", "game"));
-        var game = new Game("game", "organizer", events);
-        Assert.AreEqual("game\nhululu;asdasd;;0", game.ResultsToString());
-    }
-
-    [TestMethod]
     public void GamesToStringTest()
     {
         var events = new List<Event>();
