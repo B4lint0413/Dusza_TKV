ab04a9db087ac093a9e9b87fbbb7348a1f6270a0
B4lint0413 <69457665+B4lint0413@users.noreply.github.com>
2025-02-04 15:51:57 +0100

    UserController basics
    Needs to be joined with bets of the user but works properly so far

---
+++ DuszaTKV_BettingGame/BettingGameAPI/Controllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using BettingGameAPI.Models;
+using DuszaTKVGameLib;
+using DuszaTKVGameLib.DTOs.UserDTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BettingGameAPI.Controllers
+{
+    [Route("BettingGame/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly Repository<User> _userRepository;
+
+        public UserController(IDataStore dataStore)
+        {
+            _userRepository = dataStore.Users;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll() => Ok(_userRepository.Values);
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            User? byId = _userRepository[id];
+            if (byId != null)
+                return Ok(byId);
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateUserDto dto)
+        {
+            User user = new User(dto.Name, dto.Password);
+            _userRepository.Add(user);
+            return Ok(user);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateUserDto dto)
+        {
+            User? byId = _userRepository[id];
+            if (byId == null)
+                return NotFound();
+            byId.Points = dto.Points;
+            _userRepository.Update(byId);
+            return Ok(byId);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            User? byId = _userRepository[id];
+            if (byId == null)
+                return NotFound();
+            _userRepository.Remove(byId);
+            return NoContent();
+        }
+    }
+}
---
+++ DuszaTKV_BettingGame/DuszaWpfLib/DTOs/UserDTOs/UpdateUserDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuszaTKVGameLib.DTOs.UserDTOs
+{
+    public class UpdateUserDto
+    {
+        public int Points { get; set; }
+    }
+}
