34d62facdc615b1cf236384a48ce9624cb7079e9
B4lint0413 <69457665+B4lint0413@users.noreply.github.com>
2025-02-04 17:03:56 +0100

    Login is added
    Now proper login gives back the token and the user's id
    
    TODO: Need to handle password security properly

---
+++ DuszaTKV_BettingGame/BettingGameAPI/Controllers/LoginController.cs
@@ -0,0 +1,59 @@
+﻿using BettingGameAPI.Models;
+using BettingGameAPI.Options;
+using DuszaTKVGameLib;
+using DuszaTKVGameLib.DTOs.UserDTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.CodeDom;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace BettingGameAPI.Controllers
+{
+    [AllowAnonymous]
+    [Route("BettingGame/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly Repository<User> _userRepository;
+        private readonly JwtOptions jwtOptions;
+        public LoginController(IDataStore dataStore, IOptions<JwtOptions> options)
+        {
+            _userRepository = dataStore.Users;
+            jwtOptions = options.Value;
+        }
+
+        [HttpPost]
+        public IActionResult Login([FromBody] CreateUserDto user)
+        {
+            User? byName = _userRepository.Values.FirstOrDefault(u => u.Name == user.Name);
+            if (byName == null)
+                return BadRequest("Invalid username or password");
+            
+            User login = new User(user.Name, user.Password);
+            if (byName.Password.HashedPassword != login.Password.HashedPassword)
+                return BadRequest("Invalid username or password");
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.Key));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Sid, byName.Id.ToString()),
+                new Claim(ClaimTypes.NameIdentifier, byName.Name)
+            };
+
+            var token = new JwtSecurityToken(
+                jwtOptions.Issuer,
+                jwtOptions.Audience,
+                claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: credentials
+            );
+            var tokenStr = new JwtSecurityTokenHandler().WriteToken(token);
+            return Ok(new{Id = byName.Id, Token = tokenStr});
+        }
+    }
+}
--- DuszaTKV_BettingGame/BettingGameAPI/Program.cs
+++ DuszaTKV_BettingGame/BettingGameAPI/Program.cs
@@ -1,8 +1,28 @@
 using BettingGameAPI.Models;
+using BettingGameAPI.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("Jwt"));
+var jwt = builder.Configuration.GetSection("Jwt").Get<JwtOptions>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwt.Issuer,
+            ValidAudience = jwt.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt.Key))
+        };
+    });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
