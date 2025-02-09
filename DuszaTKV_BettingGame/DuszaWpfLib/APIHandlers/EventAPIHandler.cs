514b5e7281912d07b856043285a3486052b9d562
B4lint0413 <69457665+B4lint0413@users.noreply.github.com>
2025-02-09 12:02:06 +0100

    APIHandler for user and event

---
+++ DuszaTKV_BettingGame/DuszaWpfLib/APIHandlers/EventAPIHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Net.Http;
+using DuszaTKVGameLib.DTOs.EventDTOs;
+
+namespace DuszaTKVGameLib.APIHandlers
+{
+    public class EventAPIHandler : APIHandlerBase
+    {
+        public IEnumerable<Event> GetEvents()
+        {
+            var message = _httpClient.GetAsync($"BettingGame/Event").Result;
+            message.EnsureSuccessStatusCode();
+            string json = message.Content.ReadAsStringAsync().Result;
+            return JsonSerializer.Deserialize<IEnumerable<Event>>(json, _options)!;
+        } 
+        public Event GetEvent(int id)
+        {
+            var message = _httpClient.GetAsync($"BettingGame/Event/${id}").Result;
+            message.EnsureSuccessStatusCode();
+            string json = message.Content.ReadAsStringAsync().Result;
+            return JsonSerializer.Deserialize<Event>(json, _options)!;
+        }
+        public void CreateEvent(CreateEventDto createDto, string token)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            string json = JsonSerializer.Serialize(createDto, _options);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var message = _httpClient.PostAsync("BettingGame/Event", content).Result;
+            message.EnsureSuccessStatusCode();
+        }
+        public void UpdateEvent(int id, UpdateEventDto updateDto, string token)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            string json = JsonSerializer.Serialize(updateDto, _options);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var message = _httpClient.PutAsync($"BettingGame/Event/{id}", content).Result;
+            message.EnsureSuccessStatusCode();
+        }
+        public void DeleteBet(int id, string token)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var message = _httpClient.DeleteAsync($"BettingGame/Event/{id}").Result;
+            message.EnsureSuccessStatusCode();
+        }
+    }
+}
---
+++ DuszaTKV_BettingGame/DuszaWpfLib/APIHandlers/UserAPIHandler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using DuszaTKVGameLib.DTOs.UserDTOs;
+using System.Net.Http;
+
+namespace DuszaTKVGameLib.APIHandlers
+{
+    public class UserAPIHandler : APIHandlerBase
+    {
+        public User GetUser(int id, string token)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var message = _httpClient.GetAsync($"BettingGame/User/${id}").Result;
+            message.EnsureSuccessStatusCode();
+            string json = message.Content.ReadAsStringAsync().Result;
+            return JsonSerializer.Deserialize<User>(json, _options)!;
+        }
+        public void CreateUser(CreateUserDto createDto)
+        {
+            string json = JsonSerializer.Serialize(createDto, _options);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var message = _httpClient.PostAsync("BettingGame/User", content).Result;
+            message.EnsureSuccessStatusCode();
+        }
+        public void UpdateEvent(int id, UpdateUserDto updateDto, string token)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            string json = JsonSerializer.Serialize(updateDto, _options);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var message = _httpClient.PutAsync($"BettingGame/User/{id}", content).Result;
+            message.EnsureSuccessStatusCode();
+        }
+        public void DeleteBet(int id, string token)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var message = _httpClient.DeleteAsync($"BettingGame/User/{id}").Result;
+            message.EnsureSuccessStatusCode();
+        }
+        public LoginUserDto Login(CreateUserDto login)
+        {
+            string json = JsonSerializer.Serialize(login, _options);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var message = _httpClient.PostAsync("BettingGame/Login", content).Result;
+            message.EnsureSuccessStatusCode();
+            string response = message.Content.ReadAsStringAsync().Result;
+            return JsonSerializer.Deserialize<LoginUserDto>(response, _options)!;
+        }
+    }
+}
